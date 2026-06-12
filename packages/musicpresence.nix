2e918654373b6e80441a227d2e274903e55af069
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-06-12 02:43:17 +0200

    fix(packages): pack musicpresence binary instead of appimage

--- packages/musicpresence.nix
+++ packages/musicpresence.nix
@@ -1,12 +1,46 @@
-{ pkgs }:
+{ pkgs, xkeyboard_config, stdenv, ... }:
 
-pkgs.appimageTools.wrapType1 {
-	pname = "musicpresence";
+pkgs.stdenv.mkDerivation rec {
+	name = "musicpresence";
 	version = "2.3.5";
 
-	src = pkgs.fetchurl {
-		# need to MANUALLY update this every time since musicpresence is evil and not in nixpkgs
-		url = "https://github.com/ungive/discord-music-presence/releases/download/v2.3.5/musicpresence-2.3.5-linux-x86_64.AppImage";
-		sha256 = "sha256-M7oDxVevspA3SGuHktS8ChQAYopgIqypiVlzyE4uyqI=";
+	src = fetchTarball {
+		url = "https://github.com/ungive/discord-music-presence/releases/download/v${version}/musicpresence-${version}-linux-x86_64.tar.gz";
+		sha256 = "0zvfg7z5gh0735l880ggw0ksknsxg5i52i45hkv2s7nw5vmsfh92";
 	};
+
+	nativeBuildInputs = [ pkgs.autoPatchelfHook pkgs.makeWrapper ];
+
+	buildInputs = builtins.attrValues {
+		inherit (pkgs)
+		e2fsprogs
+		fontconfig
+		libgcc
+		libGL
+		libgpg-error
+		libxcb
+		libx11
+		wayland;
+		inherit (stdenv.cc.cc) lib;
+	};
+
+	installPhase = ''
+		runHook preInstall
+		mkdir -p $out/bin
+		cp -r usr/share $out/
+		makeWrapper $out/share/musicpresence/bin/musicpresence $out/bin/musicpresence \
+			--set XKB_CONFIG_ROOT "${xkeyboard_config}/share/X11/xkb"
+		runHook postInstall
+	'';
+
+	# for XKB_CONFIG_ROOT
+	# https://stackoverflow.com/questions/56353346/xkbcommon-error-failed-to-add-default-include-path
+
+	meta = {
+		description = "The Discord music status that works with any media player";
+		homepage = "https://github.com/ungive/discord-music-presence";
+		# todo: package the arm version for macOS too
+		platforms = [ "x86_64-linux" ];
+	};
+
 }
