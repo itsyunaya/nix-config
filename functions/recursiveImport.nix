bcc91ffb8666c74a3a37c8dcc575d7ea964483ed
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-07-04 02:08:43 +0200

    refactor: simplify rec import function

--- functions/recursiveImport.nix
+++ functions/recursiveImport.nix
@@ -1,21 +1,17 @@
 # recreation of import-tree's basic functionality
 # since i dont need most of what it offers
-{ lib, ... }: let
-	inherit (lib) hasInfix hasSuffix;
-	inherit (builtins) filter;
-in
-	dir: let
-		dirPath =
-			if builtins.isAttrs dir && dir ? outPath
-			then dir.outPath
-			else dir;
+{ lib, ... }: dir: let
+	dirPath =
+		if builtins.isAttrs dir && dir ? outPath
+		then dir.outPath
+		else dir;
 
-		files = filter
-		(file: let
-				path = toString file;
-			in
-				hasSuffix ".nix" path && !hasInfix "/_" path)
-		(lib.filesystem.listFilesRecursive dirPath);
-	in {
-		imports = files;
-	}
+	files = builtins.filter
+	(file: let
+			path = toString file;
+		in
+			lib.hasSuffix ".nix" path && !lib.hasInfix "/_" path)
+	(lib.filesystem.listFilesRecursive dirPath);
+in {
+	imports = files;
+}
