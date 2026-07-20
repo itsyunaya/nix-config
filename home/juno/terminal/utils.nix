3ffb498678aae34a56666003acf7783fb3ed5b02
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-07-20 02:47:56 +0200

    refactor(juno): move xdg mime config to nixos module

--- home/juno/terminal/utils.nix
+++ home/juno/terminal/utils.nix
@@ -1,23 +1,6 @@
 {
-	programs = {
-		eza = {
-			enable = true;
-			enableZshIntegration = true;
-		};
-
-		yazi = {
-			enable = true;
-			shellWrapperName = "y";
-
-			enableZshIntegration = true;
-		};
-
-		direnv = {
-			enable = true;
-
-			enableZshIntegration = true;
-
-			nix-direnv.enable = true;
-		};
+	programs.eza = {
+		enable = true;
+		enableZshIntegration = true;
 	};
 }
---
+++ hosts/juno/modules/xdg.nix
@@ -0,0 +1,14 @@
+{
+	xdg.mime = {
+		enable = true;
+		defaultApplications = {
+			"x-scheme-handler/http" = "zen-beta.desktop";
+			"x-scheme-handler/https" = "zen-beta.desktop";
+			"text/html" = "zen-beta.desktop";
+			"x-scheme-handler/discord" = "vesktop.desktop";
+			"x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+			"inode/directory" = "dolphin.desktop";
+			"image/*" = "qimgv.desktop";
+		};
+	};
+}
