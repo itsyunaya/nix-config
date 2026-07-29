0cc5832385cf7a3f7aa8a6333299d420e28f0e8a
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-07-29 21:56:24 +0200

    refactor(juno): move packages to their own file

--- hosts/juno/configuration.nix
+++ hosts/juno/configuration.nix
@@ -1,4 +1,4 @@
-{ fnLib, theme, inputs, pkgs, self, ... }: let
+{ fnLib, theme, pkgs, self, ... }: let
 	username = "ashley";
 in {
 	nix.settings.experimental-features = [
@@ -50,113 +50,13 @@ in {
 		uid = 1000;
 	};
 
-	environment = {
-		systemPackages = let
-			prism = pkgs.prismlauncher.override {
-				# system glfw for running mc natively on wayland
-				# only works for some versions up to 26.x
-				additionalLibs = [ pkgs.glfw ];
-				# "PRESS ENTER TO ENABLE THE NARRATOR !!!"
-				# no shut up i dont care :sob:
-				textToSpeechSupport = false;
-			};
-
-			tex-custom = pkgs.texliveSmall.withPackages (ps:
-					builtins.attrValues {
-						inherit
-							(ps)
-							scheme-medium
-							biber
-							biblatex
-							biblatex-bath
-							circuitikz
-							csquotes
-							lastpage
-							mdframed
-							needspace
-							pgfplots
-							svg
-							transparent
-							wrapfig
-							zref
-							;
-					});
-
-			awww = inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww;
-			ags-bar = inputs.ags-bar.packages.${pkgs.stdenv.hostPlatform.system}.default;
-			zen = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
-		in
-			builtins.attrValues {
-				inherit
-					ags-bar
-					awww
-					prism
-					tex-custom
-					zen
-					;
-
-				inherit
-					(pkgs.jetbrains)
-					clion
-					idea
-					webstorm
-					;
-
-				inherit
-					(pkgs)
-					alsa-utils
-					anki
-					apfs-fuse
-					aseprite
-					btop
-					cifs-utils
-					darkly
-					eza
-					ffmpeg
-					ffmpegthumbnailer
-					fzf
-					gnome-themes-extra
-					hyprshot
-					keepassxc
-					libnotify
-					mpv
-					nicotine-plus
-					obsidian
-					openssl
-					pinentry-qt
-					picard
-					playerctl
-					pnpm
-					pwvucontrol
-					qimgv
-					telegram-desktop
-					unzip
-					wget
-					whitesur-cursors
-					whitesur-icon-theme
-					wl-clipboard
-					xdg-utils
-					xlsclients
-					xwl-notifier
-					yams
-					zathura
-					;
-
-				qt6-qtwayland = pkgs.qt6.qtwayland;
-				qt5-qtwayland = pkgs.qt5.qtwayland;
-
-				qtsvg6 = pkgs.kdePackages.qtsvg;
-				qtsvg5 = pkgs.qt5.qtsvg;
-			};
-
-		sessionVariables = {
-			QT_IM_MODULE = "fcitx";
-			XMODIFIERS = "@im=fcitx";
-			SDL_IM_MODULE = "fcitx";
-			GLFW_IM_MODULE = "ibus";
-			QT_QPA_PLATFORM = "wayland";
-			NIXOS_OZONE_WL = "1";
-		};
+	environment.sessionVariables = {
+		QT_IM_MODULE = "fcitx";
+		XMODIFIERS = "@im=fcitx";
+		SDL_IM_MODULE = "fcitx";
+		GLFW_IM_MODULE = "ibus";
+		QT_QPA_PLATFORM = "wayland";
+		NIXOS_OZONE_WL = "1";
 	};
 
 	# state version should only be changed when it is really necessary,
---
+++ hosts/juno/modules/packages.nix
@@ -0,0 +1,99 @@
+{ inputs, pkgs, ... }: {
+	environment.systemPackages = let
+		prism = pkgs.prismlauncher.override {
+			# system glfw for running mc natively on wayland
+			# only works for some versions up to 26.x
+			additionalLibs = [ pkgs.glfw ];
+			# "PRESS ENTER TO ENABLE THE NARRATOR !!!"
+			# no shut up i dont care :sob:
+			textToSpeechSupport = false;
+		};
+
+		tex-custom = pkgs.texliveSmall.withPackages (ps:
+				builtins.attrValues {
+					inherit
+						(ps)
+						scheme-medium
+						biber
+						biblatex
+						biblatex-bath
+						circuitikz
+						csquotes
+						lastpage
+						mdframed
+						needspace
+						pgfplots
+						svg
+						transparent
+						wrapfig
+						zref
+						;
+				});
+
+		awww = inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww;
+		ags-bar = inputs.ags-bar.packages.${pkgs.stdenv.hostPlatform.system}.default;
+		zen = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
+	in
+		builtins.attrValues {
+			inherit
+				ags-bar
+				awww
+				prism
+				tex-custom
+				zen
+				;
+
+			inherit
+				(pkgs.jetbrains)
+				clion
+				idea
+				webstorm
+				;
+
+			inherit
+				(pkgs)
+				alsa-utils
+				anki
+				apfs-fuse
+				aseprite
+				btop
+				cifs-utils
+				darkly
+				eza
+				ffmpeg
+				ffmpegthumbnailer
+				fzf
+				gnome-themes-extra
+				hyprshot
+				keepassxc
+				libnotify
+				mpv
+				nicotine-plus
+				obsidian
+				openssl
+				pinentry-qt
+				picard
+				playerctl
+				pnpm
+				pwvucontrol
+				qimgv
+				telegram-desktop
+				unzip
+				wget
+				whitesur-cursors
+				whitesur-icon-theme
+				wl-clipboard
+				xdg-utils
+				xlsclients
+				xwl-notifier
+				yams
+				zathura
+				;
+
+			qt6-qtwayland = pkgs.qt6.qtwayland;
+			qt5-qtwayland = pkgs.qt5.qtwayland;
+
+			qtsvg6 = pkgs.kdePackages.qtsvg;
+			qtsvg5 = pkgs.qt5.qtsvg;
+		};
+}
