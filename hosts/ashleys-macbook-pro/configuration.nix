8571236be97b7c6e25a4b99c4cfca2c2a1e4f02f
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-08-03 19:23:15 +0200

    feat(macbook): add vscodium

--- hosts/ashleys-macbook-pro/configuration.nix
+++ hosts/ashleys-macbook-pro/configuration.nix
@@ -1,5 +1,24 @@
 { fnLib, pkgs, ... }: let
 	username = "ashley";
+
+	pixel-berry-theme = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+		mktplcRef = {
+			name = "pixel-berry";
+			publisher = "germainelry";
+			version = "0.1.3";
+			sha256 = "sha256-Jrs8BPY/d6F/lbyc9YcVlYON7ZirBApgzG3Tws9jaME=";
+		};
+	};
+
+	# this sucks a bit since the hjem module for it is *right there*, but i can't properly add it to my packages
+	# because linking to users.packages (what hjem does) doesn't add it to Applications/
+	vscode = pkgs.vscode-with-extensions.override {
+		vscode = pkgs.vscodium;
+		vscodeExtensions = with pkgs.vscode-extensions; [
+			jnoortheen.nix-ide
+			pixel-berry-theme
+		];
+	};
 in {
 	nix.settings.experimental-features = [
 		"nix-command"
@@ -21,6 +40,10 @@ in {
 	documentation.enable = false;
 
 	environment.systemPackages = builtins.attrValues {
+		inherit
+			vscode
+			;
+
 		inherit
 			(pkgs)
 			gnupg
@@ -48,6 +71,6 @@ in {
 			PROMPT="%{%F{#c6a0f6}%}[%{%F{#fefefe}%}%n%{%F{#c6a0f6}%}@%{%F{#fefefe}%}%m%{%F{#c6a0f6}%}] (%{%F{#fefefe}%}%1~%{%F{#c6a0f6}%}) %{%f%}$ "
 		'';
 	};
-	
+
 	system.stateVersion = 7;
 }
