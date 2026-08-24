e751393357183e43853517d23bb7a7097133b5fe
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-08-24 23:32:12 +0200

    refactor(macbook): split programs into separate file

--- hosts/ashleys-macbook-pro/configuration.nix
+++ hosts/ashleys-macbook-pro/configuration.nix
@@ -1,26 +1,5 @@
-{ fnLib, pkgs, wrappers, ... }: let
+{ fnLib, pkgs, ... }: let
 	username = "ashley";
-
-	pixel-berry-theme = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-		mktplcRef = {
-			name = "pixel-berry";
-			publisher = "germainelry";
-			version = "0.1.3";
-			sha256 = "sha256-Jrs8BPY/d6F/lbyc9YcVlYON7ZirBApgzG3Tws9jaME=";
-		};
-	};
-
-	# this sucks a bit since the hjem module for it is *right there*, but i can't properly add it to my packages
-	# because linking to users.packages (what hjem does) doesn't add it to Applications/
-	vscode = pkgs.vscode-with-extensions.override {
-		vscode = pkgs.vscodium;
-		vscodeExtensions = with pkgs.vscode-extensions; [
-			jnoortheen.nix-ide
-			pixel-berry-theme
-			haskell.haskell
-			justusadam.language-haskell
-		];
-	};
 in {
 	users = {
 		knownUsers = [ username ];
@@ -31,47 +10,19 @@ in {
 		};
 	};
 
-	imports = fnLib.fromShared [
-		"common"
-		"mnw"
-		"spicetify"
-	];
+	imports =
+		[ ./programs.nix ]
+		++ fnLib.fromShared [
+			"common"
+			"mnw"
+			"spicetify"
+		];
 
 	documentation.enable = false;
 
 	environment = {
-		systemPackages = builtins.attrValues {
-			inherit
-				vscode
-				;
-
-			inherit
-				(pkgs)
-				gnupg
-				localsend
-				pinentry_mac
-				skimpdf
-				;
-		} ++ [
-			(wrappers.git { hostName = "macbook"; })
-			wrappers.eza.drv
-		];
-
 		variables."PATH" = "$PATH:$HOME/.local/bin";
-
 		shellAliases.rb = "nh darwin switch /Users/ashley/.config/nix";
-	};
-
-	programs = {
-		fish = {
-			enable = true;
-			package = wrappers.fish.drv;
-			useBabelfish = true;
-		};
-
-		# since zsh is the default shell on macos, this is on by default
-		# and needs to be disabled to not add unnecessary files
-		zsh.enable = false;
 	};
 
 	system.stateVersion = 7;
---
+++ hosts/ashleys-macbook-pro/programs.nix
@@ -0,0 +1,51 @@
+{ pkgs, wrappers, ... }: let
+	pixel-berry-theme = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+		mktplcRef = {
+			name = "pixel-berry";
+			publisher = "germainelry";
+			version = "0.1.3";
+			sha256 = "sha256-Jrs8BPY/d6F/lbyc9YcVlYON7ZirBApgzG3Tws9jaME=";
+		};
+	};
+
+	# this sucks a bit since the hjem module for it is *right there*, but i can't properly add it to my packages
+	# because linking to users.packages (what hjem does) doesn't add it to Applications/
+	vscode = pkgs.vscode-with-extensions.override {
+		vscode = pkgs.vscodium;
+		vscodeExtensions = with pkgs.vscode-extensions; [
+			jnoortheen.nix-ide
+			pixel-berry-theme
+			haskell.haskell
+			justusadam.language-haskell
+		];
+	};
+in {
+	environment.systemPackages = builtins.attrValues {
+		inherit
+			vscode
+			;
+
+		inherit
+			(pkgs)
+			gnupg
+			localsend
+			pinentry_mac
+			skimpdf
+			;
+	} ++ [
+		(wrappers.git { hostName = "macbook"; })
+		wrappers.eza.drv
+	];
+
+	programs = {
+		fish = {
+			enable = true;
+			package = wrappers.fish.drv;
+			useBabelfish = true;
+		};
+
+		# since zsh is the default shell on macos, this is on by default
+		# and needs to be disabled to not add unnecessary files
+		zsh.enable = false;
+	};
+}
