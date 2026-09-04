e2bd5a8ef8b08780f5141854e9bddbbadf421d78
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-09-04 03:08:32 +0200

    feat: add some git related abbreviations

--- shared/wrappers/fish/abbreviations.nix
+++ shared/wrappers/fish/abbreviations.nix
@@ -4,4 +4,6 @@
 	# git
 	# see https://github.com/sharkdp/bat#git-diff
 	gd = "git diff --name-only --relative --diff-filter=d -z | xargs -0 bat --diff";
+	gs = "git status";
+	gc = "git commit -v";
 }
