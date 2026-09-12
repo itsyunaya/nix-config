dc57294b61afa4db6593f4c3f6be1dae85efaec9
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-09-12 11:32:39 +0200

    refactor: remove hyprland related components

--- hosts/juno/modules/programs/default.nix
+++ hosts/juno/modules/programs/default.nix
@@ -18,15 +18,6 @@
 			pinentryPackage = pkgs.pinentry-qt;
 		};
 
-		hyprland = {
-			enable = false;
-			package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-			portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-
-			xwayland.enable = true;
-			withUWSM = true;
-		};
-
 		mango = {
 			enable = true;
 			#package = wrappers.mangowc.drv;
--- shared/wrappers/fish/abbreviations.nix
+++ shared/wrappers/fish/abbreviations.nix
@@ -5,6 +5,7 @@
 	gs = "git status";
 	gc = "git commit -v";
 	gd = "git diff --name-only --relative --diff-filter=d -z | xargs -0 bat --diff";
-	gp = "git add --patch";
 	ga = "git add -A";
+	gap = "git add --patch";
+	gp = "git push";	
 }
